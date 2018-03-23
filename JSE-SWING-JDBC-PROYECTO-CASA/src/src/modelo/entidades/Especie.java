e1449ff77418521e5c5f1fb229033a7f9eccfeeb
Tamara <Tamara@DESKTOP-ERM0R88>
2018-03-23 19:27:17 +0100

    otra prueba

--- JSE-SWING-JDBC-PROYECTO-CASA/src/src/modelo/entidades/Especie.java
+++ JSE-SWING-JDBC-PROYECTO-CASA/src/src/modelo/entidades/Especie.java
@@ -3,13 +3,31 @@ package src.modelo.entidades;
 
 public class Especie {
     private  int id_especie;
-    private  String nombreEspecie;
-    private  String nombreGenero;
+    private  String especie_name;
+    private  String genero;
+    private  String autor;
+    private String descripcion;
+    private int imagen;
+    private String metabolismo;
+    private String ecologia;
+    private String references;
+    private int id_secuencia;
 
-    public Especie(int codigoEspecie, String nombreEspecie, String nombreGenero) {
-        this.id_especie = codigoEspecie;
-        this.nombreEspecie = nombreEspecie;
-        this.nombreGenero = nombreGenero;
+    public Especie() {
+    }
+    
+    
+
+    public Especie(int id_especie, String especie_name, String autor, String descripcion, int imagen, String metabolismo, String ecologia, String references, int id_secuencia) {
+        this.id_especie = id_especie;
+        this.especie_name = especie_name;
+        this.autor = autor;
+        this.descripcion = descripcion;
+        this.imagen = imagen;
+        this.metabolismo = metabolismo;
+        this.ecologia = ecologia;
+        this.references = references;
+        this.id_secuencia = id_secuencia;
     }
 
     public int getId_especie() {
@@ -20,25 +38,83 @@ public class Especie {
         this.id_especie = id_especie;
     }
 
-    public String getNombreEspecie() {
-        return nombreEspecie;
+    public String getEspecie_name() {
+        return especie_name;
     }
 
-    public void setNombreEspecie(String nombreEspecie) {
-        this.nombreEspecie = nombreEspecie;
+    public void setEspecie_name(String especie_name) {
+        this.especie_name = especie_name;
     }
 
-    public String getNombreGenero() {
-        return nombreGenero;
+    public String getGenero() {
+        return genero;
     }
 
-    public void setNombreGenero(String nombreGenero) {
-        this.nombreGenero = nombreGenero;
+    public void setGenero(String genero) {
+        this.genero = genero;
     }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(int imagen) {
+        this.imagen = imagen;
+    }
+
+    public String getMetabolismo() {
+        return metabolismo;
+    }
+
+    public void setMetabolismo(String metabolismo) {
+        this.metabolismo = metabolismo;
+    }
+
+    public String getEcologia() {
+        return ecologia;
+    }
+
+    public void setEcologia(String ecologia) {
+        this.ecologia = ecologia;
+    }
+
+    public String getReferences() {
+        return references;
+    }
+
+    public void setReferences(String references) {
+        this.references = references;
+    }
+
+    public int getId_secuencia() {
+        return id_secuencia;
+    }
+
+    public void setId_secuencia(int id_secuencia) {
+        this.id_secuencia = id_secuencia;
+    }
+
+    
 
     @Override
     public String toString() {
-        return  id_especie + " " + nombreEspecie + " " + nombreGenero ;
+        return  id_especie + " " + genero + " " + especie_name ;
     }
     
     
