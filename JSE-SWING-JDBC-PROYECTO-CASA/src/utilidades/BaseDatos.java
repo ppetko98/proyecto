065ef7342b1d4a0bec53fa8796860a466f782578
Tamara <Tamara@DESKTOP-ERM0R88>
2018-04-07 16:40:55 +0200

    Merge origin/master
    Conflicts:
    	JSE-SWING-JDBC-PROYECTO-CASA/src/modelo/dao/EspecieController.java
    	JSE-SWING-JDBC-PROYECTO-CASA/src/modelo/dao/Implements/EspecieControllerImpl.java
    	JSE-SWING-JDBC-PROYECTO-CASA/src/test/Ejecucion.java
    	JSE-SWING-JDBC-PROYECTO-CASA/src/test/PruebaConexion.java
    	JSE-SWING-JDBC-PROYECTO-CASA/src/utilidades/BaseDatos.java

--- JSE-SWING-JDBC-PROYECTO-CASA/src/utilidades/BaseDatos.java
+++ JSE-SWING-JDBC-PROYECTO-CASA/src/utilidades/BaseDatos.java
@@ -14,7 +14,7 @@ public interface BaseDatos {
     String DRIVERS = "com.mysql.jdbc.Driver";
     String URL = "jdbc:mysql://localhost:3306/biologia";
     String USUARIO = "root";
-    String CLAVE = "Stoyanov98";
+    String CLAVE = "root";
 
     String SELECT_ESPECIE
             = "SELECT e.id_especie, g.genero_name, e.especie_name\n"
